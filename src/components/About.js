044c8fd5186ed0f192c33d2a3a1066295ee5db33
Nana <nanaolomaiyani@gmail.com>
2022-09-22 16:37:28 +0300

    Completed Assignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,20 @@
 import React from "react";
+import Links from "./Links";
 
 function About() {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p>I made this!</p>
+  
+    
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+      <Links />
+      
       {/* add your <Links /> component here */}
+
+
+
     </div>
   );
 }
